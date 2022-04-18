57a9471ee9db1d1f3e08e6b35a9799dbfceed0a9
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2022-04-18 11:10:24 -0300

    settings

--- src/components/HomeTabBar.js
+++ src/components/HomeTabBar.js
@@ -1,38 +1,26 @@
 import React, { Component } from 'react';
 import { View, ScrollView, TouchableOpacity, Text, StyleSheet } from 'react-native'
 
-export default class HomeTabBar extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-        };
-
-        this.toGo = this.toGo.bind(this);
-    };
-
-    toGo(screenName) {
-        this.props.navigation.navigate(screenName);
-    };
-
-    render() {
-        return (
-            <View style={styles.container}>
-            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#FFFFFF' }} horizontal={true}>
-                <TouchableOpacity style={styles.buttonPage} onPress={()=>this.toGo('forYoungs')}>
-                    <Text style={[styles.textPage, {opacity: this.props.index===0 ? 1 : 0.5}]}>Jovens</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.buttonPage} onPress={()=>this.toGo('forDepartaments')}>
-                    <Text style={[styles.textPage, {opacity: this.props.index===1 ? 1 : 0.5}]}>Departamentos</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.buttonPage} onPress={()=>this.toGo('forAnalytics')}>
-                    <Text style={[styles.textPage, {opacity: this.props.index===2 ? 1 : 0.5}]}>Analytics</Text>
-                </TouchableOpacity>
-            </ScrollView>
-        </View>
-        )
+export default ({state, navigation}) => {
+    const goTo = (screenName) => {
+        navigation.navigate(screenName);
     }
+
+    return (
+        <View style={styles.container}>
+        <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#FFFFFF' }} horizontal={true}>
+            <TouchableOpacity style={styles.buttonPage} onPress={()=>goTo('forYoungs')}>
+                <Text style={[styles.textPage, {opacity: state.index===0 ? 1 : 0.5}]}>Jovens</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonPage} onPress={()=>goTo('forDepartaments')}>
+                <Text style={[styles.textPage, {opacity: state.index===1 ? 1 : 0.5}]}>Departamentos</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonPage} onPress={()=>goTo('forAnalytics')}>
+                <Text style={[styles.textPage, {opacity: state.index===2 ? 1 : 0.5}]}>Analytics</Text>
+            </TouchableOpacity>
+        </ScrollView>
+    </View>
+    )
 }
 
 const styles = StyleSheet.create({
--- src/screens/InsertYoungs.js
+++ src/screens/InsertYoungs.js
@@ -1,22 +1,152 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, Animated, TextInput, StyleSheet } from 'react-native';
+import { TextInputMask } from 'react-native-masked-text';
+import MQV from '../assets/Logo.svg';
+import Back from '../assets/nav_prev.svg';
+
+import Email from '../assets/email.svg';
+import Person from '../assets/person.svg';
+import Whats from '../assets/telefone-celular.svg';
+import Doc from '../assets/pasta-de-documentos.svg';
+import Schedule from '../assets/today.svg';
+import Instagram from '../assets/instagram.svg';
 
 export default class InsertYoungs extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            name: '',
+            mail: '',
+            date: '',
+            phone: '',
+            instagram: '',
+            genre: ''
         };
     };
 
     render() {
         return (
             <View style={styles.background}>
-                <Text>InsertYoungs</Text>
-                <TouchableOpacity onPress={()=>{ this.props.navigation.navigate('Home') }}>
-                    <Text>Voltar</Text>
-                </TouchableOpacity>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={()=>{ this.props.navigation.navigate('Home') }}>
+                        <Back width="40" height="40" fill="#000000"/>
+                    </TouchableOpacity>
+                    <Text style={styles.title}>MQV</Text>
+                    <MQV width="46" height="46" style={styles.icon}/>
+                </View>
+                <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+                    <View style={styles.titleArea}>
+                        <Text style={styles.subtitle}>Cadastre o Jovem</Text>
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Person width="24" height="24" fill="#9B111E" />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Nome"
+                            placeholderTextColor="#000000"
+                            value={this.state.name}
+                            autoCapitalize='none'
+                            onChangeText={(name)=>{ this.setState({ name }) }} 
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Email width="24" height="24" fill="#9B111E" />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Email"
+                            placeholderTextColor="#000000"
+                            value={this.state.mail}
+                            autoCapitalize='none'
+                            onChangeText={(mail)=>{ this.setState({ mail }) }} 
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Schedule width="24" height="24" fill="#9B111E" />
+                        <TextInputMask
+                            type={'datetime'}
+                            options={{
+                                format: 'DD/MM/YYYY'
+                            }}
+                            placeholder="Data de nascimento"
+                            placeholderTextColor="#000000"
+                            value={this.state.date}
+                            style={styles.TextMasked}
+                            onChangeText={(date)=>{ this.setState({ date }) }}
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Whats width="24" height="24" fill="#9B111E" />
+                        <TextInputMask
+                            type={'cel-phone'}
+                            options={{
+                                maskType: 'BRL',
+                                withDDD: true,
+                                dddMask: '(99)'
+                            }}
+                            placeholder="WhatsApp"
+                            placeholderTextColor="#000000"
+                            style={styles.TextMasked}
+                            value={this.state.phone}
+                            onChangeText={(phone)=>{ this.setState({ phone }) }}
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Whats width="24" height="24" fill="#9B111E" />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Instagram"
+                            placeholderTextColor="#000000"
+                            value={this.state.instagram}
+                            autoCapitalize='none'
+                            onChangeText={(instagram)=>{ this.setState({ instagram }) }} 
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Email width="24" height="24" fill="#9B111E" />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Digite seu nome"
+                            placeholderTextColor="#000000"
+                            value={this.state.name}
+                            autoCapitalize='none'
+                            onChangeText={(name)=>{ this.setState({ name }) }} 
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Email width="24" height="24" fill="#9B111E" />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Digite seu nome"
+                            placeholderTextColor="#000000"
+                            value={this.state.name}
+                            autoCapitalize='none'
+                            onChangeText={(name)=>{ this.setState({ name }) }} 
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Email width="24" height="24" fill="#9B111E" />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Digite seu nome"
+                            placeholderTextColor="#000000"
+                            value={this.state.name}
+                            autoCapitalize='none'
+                            onChangeText={(name)=>{ this.setState({ name }) }} 
+                        />
+                    </View>
+                    <View style={styles.inputArea}>
+                        <Email width="24" height="24" fill="#9B111E" />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Digite seu nome"
+                            placeholderTextColor="#000000"
+                            value={this.state.name}
+                            autoCapitalize='none'
+                            onChangeText={(name)=>{ this.setState({ name }) }} 
+                        />
+                    </View>
+                </ScrollView>
             </View>
         );
     };
@@ -27,5 +157,65 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    header: {
+        height: 130,
+        width: 415,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginBottom: 10,
+        paddingTop: 60,
+        backgroundColor: '#9B111E'
+    },
+    title: {
+        fontSize: 24,
+        color: '#000',
+        fontWeight: 'bold'
+    },
+    icon: {
+        width: 36,
+        height: 36,
+    },
+    pageBody: {
+        height: 400,
+        width: 400,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    inputArea: {
+        width: 350,
+        height: 50,
+        backgroundColor: '#FFFFFF',
+        flexDirection: 'row',
+        borderRadius: 10,
+        paddingLeft: 15,
+        alignItems: 'center',
+        borderWidth: 3,
+        borderColor: '#000000',
+        margin: 3
+    },
+    input: {
+        flex: 1,
+        fontSize: 16,
+        color: '#000000',
+        marginLeft: 5
+    },
+    TextMasked: {
+        flex: 1,
+        fontSize: 16,
+        color: "#000000",
+        marginLeft: 5
+    },
+    titleArea: {
+        width: 350,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    subtitle: {
+        fontSize: 20,
+        color: '#000',
+        fontWeight: 'bold'
     }
 });
